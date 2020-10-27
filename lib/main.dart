c6c5143eb483a90421ddb306df483ff2fec69f04
Tokaji Koki <kd1262089@st.kobedenshi.ac.jp>
2020-10-27 19:09:24 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,47 @@
-import 'package:coriander/next_page.dart';
+import 'package:coriander/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String kboyText = 'KBOY';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items = ['kboy1','kboy2','kboy3','4',];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KBOYさんじゃないよ！！'),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.red,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.green,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.orange,
-            ),
-          ],
-        )
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        //ここでなにか
+                        model.changeKboyText();
+                      },
+                    )
+                  ],
+                ),
+              );
+            }
+          ),
+        ),
       ),
     );
   }
 }
+
